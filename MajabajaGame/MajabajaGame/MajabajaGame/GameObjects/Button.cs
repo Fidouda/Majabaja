150f10da94493d4de437b3125bde7ab193c4c14d
Benjamin Brodeur <benjaminbrodeur@gmail.com>
2014-01-18 12:09:37 -0500

    Les boutons sont implémentés et fonctionnent

--- MajabajaGame/MajabajaGame/MajabajaGame/GameObjects/Button.cs
+++ MajabajaGame/MajabajaGame/MajabajaGame/GameObjects/Button.cs
@@ -11,96 +11,28 @@ public enum ButtonStatus {Normal, Highlighted, Clicked}
 
 public class Button
 {
-    Texture2D image;
-    SpriteFont font;
-    Rectangle location;
-    string text;
-    Vector2 textLocation;
-    SpriteBatch spriteBatch;
-    MouseState mouse;
-    MouseState oldMouse;
-    bool clicked = false;
-    string clickText = "Button was Clicked!";
-    public event EventHandler Clicked;
+    Texture2D m_image;
+    Rectangle m_field;
+    Vector2 m_position;
+    SpriteBatch m_spriteBatch;
 
-    public Button(Texture2D texture, SpriteFont font, SpriteBatch sBatch)
+    public Button(SpriteBatch p_spriteBatch, Texture2D p_image, Vector2 p_position )
     {
-        image = texture;
-        this.font = font;
-        location = new Rectangle(0, 0, image.Width, image.Height);
-        spriteBatch = sBatch;
+        m_spriteBatch = p_spriteBatch;
+        m_image = p_image;
+        m_position = p_position;
+        m_field = new Rectangle((int)m_position.X, (int)m_position.Y, (int)m_image.Width, (int)m_image.Height);
     }
 
-    public string Text
+
+    public Rectangle getField()
     {
-        get { return text; }
-        set
-        {
-            text = value;
-            Vector2 size = font.MeasureString(text);
-            textLocation = new Vector2();
-            textLocation.Y = location.Y + ((image.Height / 2) - (size.Y / 2));
-            textLocation.X = location.X + ((image.Width / 2) - (size.X / 2));
-        }
-    }
-
-    public void Location(int x, int y)
-    {
-        location.X = x;
-        location.Y = y;
-    }
-    
-
-    /// <summary>
-    /// Allows the game component to update itself.
-    /// </summary>
-    /// <param name="gameTime">Provides a snapshot of timing values.</param>
-    public void Update()
-    {
-        mouse = Mouse.GetState();
-
-        if (mouse.LeftButton == ButtonState.Released && oldMouse.LeftButton == ButtonState.Pressed)
-        {
-            if (location.Contains(new Point(mouse.X, mouse.Y)))
-            {
-                clicked = true;
-            }
-        }
-
-        Text = "Play";
-        oldMouse = mouse;
+        return m_field;
     }
 
 
     public void Draw()
     {
-
-        if (location.Contains(new Point(mouse.X, mouse.Y)))
-        {
-            spriteBatch.Draw(image,
-                location,
-                Color.Silver);
-        }
-        else
-        {
-            spriteBatch.Draw(image,
-                location,
-                Color.White);
-        }
-
-        spriteBatch.DrawString(font,
-            text,
-            textLocation,
-            Color.Black);
-
-        if (clicked)
-        {
-            Vector2 position = new Vector2(10, 75);
-            spriteBatch.DrawString(font,
-                clickText,
-                position,
-                Color.White);
-        }
+        m_spriteBatch.Draw(m_image, m_position, Color.White);
     }
-
 }
